344fd6507bb62db06f88c92d0abf633324611171
AliasTheBlank <aliastheblank@gmail.com>
2023-09-27 20:38:32 -0400

    feat: tile board of entities added

--- ChessGame/Scenes/CGGameScene.cs
+++ ChessGame/Scenes/CGGameScene.cs
@@ -1,62 +1,44 @@
-﻿using ChessGame.Actors;
+﻿using System.Linq;
+using ChessGame.Actors;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Nez;
+using Nez.Sprites;
+using Nez.Textures;
 
 namespace ChessGame.Scenes;
 
-public class CGGameScene : Scene, IFinalRenderDelegate
+public class CGGameScene : Scene
 {
     public const int ScreenSpaceRenderLayer = 999;
     
     ScreenSpaceRenderer _screenSpaceRenderer;
     static bool _needsFullRenderSizeForUi;
+    private CGTile Board;
+    public CGTile[,] TileBoard;
     
     public UICanvas Canvas;
 
     
-    public CGGameScene(bool addExcludeRenderer = true, bool needsFullRenderSizeForUi = false)
+    public CGGameScene()
     {
-        _needsFullRenderSizeForUi = needsFullRenderSizeForUi;
         
-
-        // setup one renderer in screen space for the UI and then (optionally) another renderer to render everything else
-        if (needsFullRenderSizeForUi)
-        {
-            // dont actually add the renderer since we will manually call it later
-            _screenSpaceRenderer = new ScreenSpaceRenderer(100, ScreenSpaceRenderLayer);
-            _screenSpaceRenderer.ShouldDebugRender = false;
-            FinalRenderDelegate = this;
-        }
-        else
-        {
-            AddRenderer(new ScreenSpaceRenderer(100, ScreenSpaceRenderLayer));
-        }
-
-        if (addExcludeRenderer)
-            AddRenderer(new RenderLayerExcludeRenderer(0, ScreenSpaceRenderLayer));
-
+        int column = 65;
+        int row = 1;
+        TileBoard = new CGTile[8, 8];
+        
+        
+        Screen.SetSize(640*2, 640*2);
+    
         // create our canvas and put it on the screen space render layer
         Canvas = CreateEntity("ui").AddComponent(new UICanvas());
         Canvas.IsFullScreen = true;
         Canvas.RenderLayer = ScreenSpaceRenderLayer;
-    }
-    
-    private Scene _scene;
 
-    public void OnAddedToScene(Scene scene) => _scene = scene;
 
-    public void OnSceneBackBufferSizeChanged(int newWidth, int newHeight) => _screenSpaceRenderer.OnSceneBackBufferSizeChanged(newWidth, newHeight);
+        var texture2 = Content.LoadTexture(@"Content/Sprites/WhiteTile.png");
+        //var tile = AddEntity(new CGTile('F',1,texture2));
+        var board = AddEntity(new CGBoard(this));
 
-    public void HandleFinalRender(RenderTarget2D finalRenderTarget, Color letterboxColor, RenderTarget2D source,
-        Rectangle finalRenderDestinationRect, SamplerState samplerState)
-    {
-        Core.GraphicsDevice.SetRenderTarget(null);
-        Core.GraphicsDevice.Clear(letterboxColor);
-        Graphics.Instance.Batcher.Begin(BlendState.Opaque, samplerState, DepthStencilState.None, RasterizerState.CullNone, null);
-        Graphics.Instance.Batcher.Draw(source, finalRenderDestinationRect, Color.White);
-        Graphics.Instance.Batcher.End();
-
-        _screenSpaceRenderer.Render(_scene);
     }
 }
