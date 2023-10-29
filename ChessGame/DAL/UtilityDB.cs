56d6747ff3a3a8f762f5fd9f4f947c4bd4d5824f
AliasTheBlank <aliastheblank@gmail.com>
2023-10-29 17:19:09 -0400

    feat: added user login validation

---
+++ ChessGame/DAL/UtilityDB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Npgsql;
+
+namespace ChessGame.DAL;
+
+public class UtilityDB
+{
+    private static string connectionString =
+        "Host=70.48.140.178:47020;Username=projectuser;Password=CodeMonkeys1;Database=ChessGameDB";
+
+
+    public static bool ValidatePlayer(string username, string password)
+    {
+        using var conn = GetConnection();
+        using var cmd =
+            new NpgsqlCommand("Select Count(*) from players where Username = (@pUsername) and Password = (@pPassword)", conn)
+            {
+                Parameters =
+                {
+                    new("@pUsername", username),
+                    new("@pPassword", password)
+                }
+            };
+
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                if (Convert.ToInt32(reader["count"].ToString()) == 1)
+                {
+                    conn.Close();
+                    return true;
+                } 
+            }
+        }
+        
+        conn.Close();
+        return false;
+    }
+
+    private static NpgsqlConnection GetConnection()
+    {
+        using var dataSource = NpgsqlDataSource.Create(connectionString);
+        
+        return dataSource.OpenConnection();
+    }
+}
---
+++ ChessGame/UI/LogInUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Forms;
+using ChessGame.DAL;
+using ChessGame.Scenes;
+using Microsoft.Xna.Framework;
+using Nez;
+using Nez.ImGuiTools;
+using Nez.Tweens;
+using Nez.UI;
+
+
+namespace ChessGame.UI;
+
+public class LogInUI : UICanvas
+{
+    public override void OnAddedToEntity()
+    {
+        base.OnAddedToEntity();
+        
+        var _table = Stage.AddElement(new Table());
+        _table.SetFillParent(true).Center();
+        
+        var skin = Skin.CreateDefaultSkin();
+        
+        var topButtonStyle = new TextButtonStyle(new PrimitiveDrawable(Color.Black, 10f),
+            new PrimitiveDrawable(Color.Yellow), new PrimitiveDrawable(Color.DarkSlateBlue))
+        {
+            DownFontColor = Color.Black
+        };
+
+        topButtonStyle.FontScale = 1.5f;
+        
+        var _lblUserName = _table.Add(new Nez.UI.Label("Username"));
+        _table.Row().SetPadTop(20);
+        
+        var _userName = new TextField("Username", skin);
+        _table.Add(_userName);
+        
+        _table.Row().SetPadTop(20);
+        var _lblLastName = _table.Add(new Nez.UI.Label("LastName"));
+        _table.Row().SetPadTop(20);
+
+        var _lastName = new TextField("LastName", skin);
+        _table.Add(_lastName);
+        _table.Row().SetPadTop(20);
+
+        _table.Add(new TextButton("Log in", topButtonStyle)).SetFillX().SetMinHeight(50)
+            .GetElement<TextButton>().OnClicked += butt =>
+        {
+            if (!UtilityDB.ValidatePlayer(_userName.GetText(), _lastName.GetText()))
+                return;
+            
+            TweenManager.StopAllTweens();
+            Core.GetGlobalManager<ImGuiManager>()?.SetEnabled(true);
+            Core.StartSceneTransition(new FadeTransition(() => new MenuScene()));
+        };
+    }
+}
